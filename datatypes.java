478c8a861afdcb82aa75a14c070be3fc54e9e446
YogeshJaiman <YogeshJaiman24@gmail.com>
2022-10-14 09:15:14 +0530

    first commit

---
+++ datatypes.java
@@ -0,0 +1,9 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int num = 123;
+        char name = 'D';
+        // String fullname = "rahul sharma";
+        boolean flag = false ;
+        double per = 70.89;
+    }
+}
